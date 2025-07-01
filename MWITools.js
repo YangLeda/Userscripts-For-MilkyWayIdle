71790443828faf6ee0012ab0d5b53b95d7c8e716
Cheng-Yang Tsai <greydust@gmail.com>
2025-07-01 11:49:09 +0100

    Minor number change

--- MWITools.js
+++ MWITools.js
@@ -4101,7 +4101,7 @@
         return validateMarketJsonFetch(MARKET_JSON_LOCAL_BACKUP, false);
     }
 
-    function numberFormatter(num, digits = 1) {
+    function numberFormatter(num, digits = 2) {
         if (num === null || num === undefined) {
             return null;
         }
